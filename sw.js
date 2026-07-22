fab203c743a067468ec06628acf32d27cb4640d7
jmanuelgonzalezlagar4-crypto <jmanuelgonzalezlagar4@gmail.com>
2026-07-22 22:02:22 +0200

    Añadir app PresuVolt (PWA)

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE = "presuvolt-v2";
+const FICHEROS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FICHEROS)).then(() => self.skipWaiting()));
+});
+self.addEventListener("activate", e => {
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    fetch(e.request).then(resp => {
+      const copia = resp.clone();
+      caches.open(CACHE).then(c => c.put(e.request, copia)).catch(() => {});
+      return resp;
+    }).catch(() => caches.match(e.request).then(r => r || caches.match("./index.html")))
+  );
+});
