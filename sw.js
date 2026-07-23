6bf9511a7b98b287db80d79371e1ddd35c46f350
jmanuelgonzalezlagar4-crypto <jmanuelgonzalezlagar4@gmail.com>
2026-07-23 08:03:02 +0200

    Fase 1 Pro: autosave, base de precios por familias y Copiloto

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "presuvolt-v3";
+const CACHE = "presuvolt-v4";
 const FICHEROS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FICHEROS)).then(() => self.skipWaiting()));
