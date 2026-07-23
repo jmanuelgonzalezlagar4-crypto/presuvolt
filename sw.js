9242ddb7c2ea06fdfa8117d666462825c4ff0c03
jmanuelgonzalezlagar4-crypto <jmanuelgonzalezlagar4@gmail.com>
2026-07-23 11:04:03 +0200

    Nuevo diseño de presupuesto «Tarjetas» elegido por Jose (v16)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "presuvolt-v15";
+const CACHE = "presuvolt-v16";
 const FICHEROS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FICHEROS)).then(() => self.skipWaiting()));
