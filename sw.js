df91ecb4327cee8edf593116fc1bd9338ed967cd
jmanuelgonzalezlagar4-crypto <jmanuelgonzalezlagar4@gmail.com>
2026-07-23 13:06:55 +0200

    Versión visible en Ajustes y actualización rápida del service worker (v20)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "presuvolt-v19";
+const CACHE = "presuvolt-v20";
 const FICHEROS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FICHEROS)).then(() => self.skipWaiting()));
